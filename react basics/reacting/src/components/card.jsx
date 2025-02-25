f1d00624b77663fcbaf526cc33c4c28dba0adec0
Sreedhar <sree.ewce@gmail.com>
2025-02-25 16:32:04 +0530

    frontend cohort added to github

---
+++ react basics/reacting/src/components/card.jsx
@@ -0,0 +1,11 @@
+function Card (){
+    return(
+        <>
+            <div className="">
+                <img src="https://source/unsplash.com/1600X1000/?man" alt="" />
+            </div>
+        </>
+    )
+}
+
+export default Card;
